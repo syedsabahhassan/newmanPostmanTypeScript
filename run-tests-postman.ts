48702400ce34ae0de8d3985b8ff315a0c84ed06a
Syed Sabah Hassan <sabahcomp@gmail.com>
2021-10-29 11:53:20 +1100

    [Sabah]Runner script

---
+++ run-tests-postman.ts
@@ -0,0 +1,23 @@
+import newman from "newman"
+
+
+let listOfCollections :string[] = [];
+
+const runner = async()=>{
+    
+
+};
+
+runner()
+    .then(console.log)
+    .catch(console.error);
+
+
+
+
+
+
+
+
+
+
