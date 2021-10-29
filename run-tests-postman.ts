032afd154478b015178db5ba3b2f6629583f8749
Syed Sabah Hassan <sabahcomp@gmail.com>
2021-10-29 18:48:43 +1100

    [Sabah]Added tsconfig and updates to package.json

--- run-tests-postman.ts
+++ run-tests-postman.ts
@@ -1,10 +1,70 @@
-import newman from "newman"
+import newman from "newman";
+import fs from "fs"; 
+
+let listOfCollections: string[] = [];
 
 
-let listOfCollections :string[] = [];
+const runner = async () => {
+    try {
+        console.info(`Starting test runner`);
+        const configFile        = "./environments/test.postman_environment.json";
+        let configData          =   fs.readFileSync(configFile,"utf8");
+        const configDataJson    =   JSON.parse(configData);
+        const testCollections   = "./exploratory";
 
-const runner = async()=>{
-    
+        let fileExtension       =   ".postman_collection.json";
+        
+        listOfCollections       =   listOfCollections.concat(
+            fs.readdirSync(testCollections).filter((fn)=>fn.endsWith(fileExtension))
+        )
+        
+        listOfCollections.forEach(element => {
+            console.info(`Running ${element} collection first`);
+            newman
+                .run({
+                    collection:require(`${testCollections}/${element}`),
+                    environment: configDataJson,
+                    insecure: true,
+                    reporters:["cli","htmlextra"],
+                    reporter:{
+                        htmlextra: {
+                            export:`./reports/${element}-executionResults.html`                        }
+                    }
+                })
+                .on("start",()=>{
+                    console.info(`****Execution of ${element} collection started`)
+                })
+                .on("done",(err,summary)=>{
+                    if(err||(summary.run.failures && summary.run.failures.length >0)){
+                        console.error(`Logging error found for ${element}`);
+                    }
+                    else{
+                        console.info(`Execution of ${element} - Completed`);
+                    }
+                })
+                .on("exception",(err)=>{
+                    console.error(`Exception for ${element}`);
+                })
+                .on("console",(err,logs)=>{
+                    console.log(logs.messages);
+                });
+
+
+        });
+        
+
+
+
+
+
+
+
+
+
+
+    } catch {
+        console.log(`Exception found while running collections through Newman`)
+    }
 
 };
 
